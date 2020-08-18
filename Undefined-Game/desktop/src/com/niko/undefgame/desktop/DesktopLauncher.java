d331d689b5679c5738dfc1f63c06e4a7c92a08c2
Nikkcoder <nikogang72@gmail.com>
2020-08-18 19:40:18 -0300

    Primer Commit

---
+++ Undefined-Game/desktop/src/com/niko/undefgame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.niko.undefgame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.niko.undefgame.UndefinedCore;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UndefinedCore(), config);
+	}
+}
