eb4c0188a4da6a9eb50c11dfa52e1f662c1f95bd
Nikkcoder <nikogang72@gmail.com>
2020-08-19 02:04:05 -0300

    Cambios varios

--- Undefined-Game/core/src/com/niko/undefgame/UndefinedCore.java
+++ Undefined-Game/core/src/com/niko/undefgame/UndefinedCore.java
@@ -1,29 +1,66 @@
 package com.niko.undefgame;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.VertexAttributes;
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Material;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.niko.sistemas.Camara;
+import com.niko.sistemas.PlayerInput;
 
-public class UndefinedCore extends ApplicationAdapter {
-	SpriteBatch batch;
+public class UndefinedCore extends Game {
+	
+	public Camara camara;
+	public PlayerInput playerInput;
+	
+	public Model model;
+	public ModelInstance instance;
+	public ModelBatch modelBatch;
+	public Environment environment;
+	
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		// inicializamos la camara
+		camara = new Camara(); 
+		
+		ModelBuilder modelBuilder = new ModelBuilder();
+		Material mat = new Material(ColorAttribute.createDiffuse(Color.RED));
+		model = modelBuilder.createBox(7, 7, 7, mat, VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+		instance = new ModelInstance(model);
+		// inicializamos el movimiento del jugador
+		playerInput = new PlayerInput(instance);
+		
+		
+		modelBatch = new ModelBatch();
+		environment = new Environment();
+		environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
 	}
+	
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.end();
+		playerInput.handler();
+		
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+		
+		modelBatch.begin(camara.cam);
+		modelBatch.render(instance, environment);
+		modelBatch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		model.dispose();
 	}
 }
