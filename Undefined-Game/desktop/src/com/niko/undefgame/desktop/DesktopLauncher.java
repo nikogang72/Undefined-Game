8e74a24807592eb261e0e8097d95ee3dada33c30
Nikkcoder <nikogang72@gmail.com>
2020-08-19 01:47:57 -0300

    Algunas configuraciones para el Launcher

--- Undefined-Game/desktop/src/com/niko/undefgame/desktop/DesktopLauncher.java
+++ Undefined-Game/desktop/src/com/niko/undefgame/desktop/DesktopLauncher.java
@@ -2,11 +2,17 @@ package com.niko.undefgame.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.niko.cfg.cfg;
 import com.niko.undefgame.UndefinedCore;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Undefined Game";
+		config.resizable = false;
+		config.foregroundFPS = cfg.fps;
+		config.width = cfg.width;
+		config.height = cfg.height;
 		new LwjglApplication(new UndefinedCore(), config);
 	}
 }
