a0aa6af1b50f7af18136b29fcc804f46e0afe707
Nikkcoder <nikogang72@gmail.com>
2020-08-19 02:01:10 -0300

    Clase estática Configuration

---
+++ Undefined-Game/core/src/com/niko/cfg/cfg.java
@@ -0,0 +1,14 @@
+package com.niko.cfg;
+
+public class cfg {
+	/** angulo de FoV de la Camara**/
+	public static float FoV = 67;
+	
+	/** Maximo de FPS permitido para la aplicación **/
+	public static int fps = 60;
+	
+	/** Ancho de la pantalla **/
+	public static int width = 1280;
+	/** Alto de la pantalla **/
+	public static int height = 720;
+}
